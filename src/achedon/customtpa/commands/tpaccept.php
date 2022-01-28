f2de9df73917725b9f6558a9c19d82431ad70eb5
leoderoin <leoderoinleo@gmail.com>
2022-01-28 17:14:41 +0100

    [UPDATE] patch for poggit

--- src/achedon/customtpa/commands/tpaccept.php
+++ src/achedon/customtpa/commands/tpaccept.php
@@ -13,7 +13,6 @@ use pocketmine\Server;
 
 class tpaccept extends Command{
 
-
     public function __construct(string $name, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = []){
         parent::__construct($name, $description, $usageMessage, $aliases);
     }
